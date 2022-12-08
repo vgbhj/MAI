c5a25280b44849714a696720226e086e44658a71
Parfenov Mikhail <mishaslsk@gmail.com>
2022-12-08 13:46:08 +0300

    Done couse project 3

---
+++ Informatics/Labs/cp3/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+// double teylor(double d, int iter){
+//     iter++;
+//     if (iter=100){
+//         return d;
+//     }
+// }
+
+int main(){
+    double ans, f;
+    double eps = 1;
+    while (1 + eps/2 > 1) eps /= 2;
+    printf("Машинное эпсилон для типа double = %.16e\n", eps);
+    int n, cnt;
+    int k = 50;
+    printf("Введите число разбиений отрезка: ");
+    scanf("%d", &n);
+    double a = 0.1;
+    double b = 0.6;
+    double step = (b - a)/n;
+    printf("Таблица значений ряда Тейлора и стандартной функции для f(x) = (1+x^2)/2*arctg(x)-x/2\n");
+    printf("================================================================== \n");
+    printf("|   x  |         sum         |      f(x)          |число итераций|\n");
+    printf("================================================================== \n");
+    double x = 0;
+    for(int i=0; i<=n; ++i){
+        double d = 1;
+        x += step;
+        ans = 0; // част сумма ряда
+        cnt = 1; // число иттераций
+        f = (1+x*x)/2*atan(x)-x/2;
+        
+        while (cnt < 50) {
+            d = pow((-1), cnt - 1) * pow(x,2*cnt+1)/(4*cnt*cnt-1);
+            // printf("%d ", d);
+            ans += d;
+            cnt++;
+        }
+        
+        printf("| %.2f | %.16f | %.16f |      %d       |\n", x, ans, f, cnt);
+    printf("=================================================================== \n");
+    }
+    
+    return 0;
+}
--- Informatics/Labs/lab11/main.c
+++ Informatics/Labs/lab11/main.c
@@ -2,6 +2,21 @@
 #include <ctype.h>
 #include <stdio.h>
 
+void for_alph(int n, char c, int sign){
+    if(n != 0){
+            if(c == 'C'){
+                c = 'F';
+                n = n*9/5*sign + 32;
+                printf("%d%c", n, c);
+                n = 0;
+                sign = 1;
+            }
+        }
+        else{
+            printf("%c", c);
+        }
+}
+
 int main(){
     char c;
     int n = 0;
@@ -17,18 +32,8 @@ int main(){
             n += c - '0';
         }
         else if(isalpha(c)){
-            if(n != 0){
-                if(c == 'C'){
-                    c = 'F';
-                    n = n*9/5*sign + 32;
-                    printf("%d%c", n, c);
-                    n = 0;
-                    sign = 1;
-                }
-            }
-            else{
-                printf("%c", c);
-            }
+            for_alph(n, c, sign);
+            
         }
         else{
             printf("%c", c);
