fd367e4a49975a601f56fb1200622d7eb9d9a429
Parfenov Mikhail <mishaslsk@gmail.com>
2024-05-30 17:41:02 +0300

    all done

--- DA/lab2-3/solution/src/RBTree.h
+++ DA/lab2-3/solution/src/RBTree.h
@@ -1,63 +1,53 @@
 #ifndef RED_BLACK_TREE_RBTREE_H
 #define RED_BLACK_TREE_RBTREE_H
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-enum Color {RED, BLACK, DOUBLE_BLACK};
+typedef bool Tcolor;
+const Tcolor RED = true;
+const Tcolor BLACK = false;
 
-struct Node
-{
-    std::string key;
-    unsigned long long value;
-    int color;
-    Node *left, *right, *parent;
+typedef unsigned long long ull;
 
-    Node(const std::string& k, unsigned long long v) 
-        : key(k)
-        , value(v)
-        , color(RED)
-        , left(nullptr)
-        , right(nullptr)
-        , parent(nullptr) 
-        {}
+struct Node{
+    Node * parent;
+    Node * left;
+    Node * right;
+    Tcolor color;
+    string key;
+    ull value;
 };
+
 
 class RBTree
 {
     private:
-        Node *root;
+        Node *NullNode;
     protected:
-        void rotateLeft(Node *&);
-        void rotateRight(Node *&);
-        void fixInsertRBTree(Node *&);
-        void fixDeleteRBTree(Node *&);
-        void inorderBST(Node *&);
-        void preorderBST(Node *&);
-        int getColor(Node *&);
-        void setColor(Node *&, int);
-        Node *minValueNode(Node *&);
-        Node *maxValueNode(Node *&);
-        Node* insertBST(Node *&, Node *&);
-        Node* deleteBST(Node *&, string);
-        Node* searchHelper(Node *, string);
-        int getBlackHeight(Node *);
-        static std::string toLower(std::string str);
-        void saveHelper(std::ofstream &outFile, Node* node);
-        void loadHelper(std::ifstream &inFile);
-        void clearHelper(Node* &node);
+        void rotateLeft(Node *);
+        void rotateRight(Node *);
+        Node *minRightNode(Node *);
+        Node *maxLeftNode(Node *);
+        void fixInsertRBTree(Node *);
+        void fixDeleteRBTree(Node *);
+        int compareString(const string &, const string &);
     public:
+        Node *root;
         RBTree();
-        void insertValue(string, unsigned long long);
-        void deleteValue(string);
-        void inorder();
-        void preorder();
-        unsigned long long search(string);
-        bool searchBool(string);
-        void saveToFile(const std::string& filename);
-        void loadFromFile(const std::string& filename);
-        void clear();
-        void getRoot();
+        ~RBTree();
+        string insertValue(const string&, ull);
+        string deleteValue(const string&);
+        
+        void erace(Node * x);
+        void get(const string &key);
+
+        void saveFile(ofstream &file, Node * x);
+        void loadFile(ifstream &file);
+
+        void print(Node * x, int level);
+        
+        static std::string toLower(std::string str);
 };
 
 
--- Olimp_prog/main.py
+++ Olimp_prog/main.py
@@ -1,2 +1,16 @@
-for i in range(25):
-    for j in range(25):
+L = []
+for i in range(300, 400):
+    L.append("+ " + "a"*i + " 1\n")
+# for i in range(1, 200):
+#     L.append("+ " + "b"*i + " 1\n")
+# for i in range(1, 200):
+#     L.append("+ " + "c"*i + " 1\n")
+# for i in range(1, 10):
+#     L.append("- " + "a"*i + "\n")
+# for i in range(1, 200-1):
+#     L.append("- " + "b"*i + "\n")
+# for i in range(1, 200-1):
+#     L.append("- " + "c"*i + "\n")
+
+with open("/home/miha/Documents/MAI/DA/lab2-3/solution/input2.txt", "w") as file1:
+    file1.writelines(L)
