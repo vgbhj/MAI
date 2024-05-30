9ce83ca9842285207d0318a5e42ff9bddd425801
Parfenov Mikhail <mishaslsk@gmail.com>
2024-05-30 21:23:51 +0300

    Add reports

--- Olimp_prog/main.py
+++ Olimp_prog/main.py
@@ -1,16 +1,16 @@
 L = []
-for i in range(300, 400):
+for i in range(1, 250):
     L.append("+ " + "a"*i + " 1\n")
-# for i in range(1, 200):
-#     L.append("+ " + "b"*i + " 1\n")
-# for i in range(1, 200):
-#     L.append("+ " + "c"*i + " 1\n")
-# for i in range(1, 10):
-#     L.append("- " + "a"*i + "\n")
-# for i in range(1, 200-1):
-#     L.append("- " + "b"*i + "\n")
-# for i in range(1, 200-1):
-#     L.append("- " + "c"*i + "\n")
+for i in range(1, 250):
+    L.append("+ " + "b"*i + " 1\n")
+for i in range(1, 250):
+    L.append("+ " + "c"*i + " 1\n")
+for i in range(1, 250):
+    L.append("- " + "a"*i + "\n")
+for i in range(1, 250):
+    L.append("- " + "b"*i + "\n")
+for i in range(1, 250):
+    L.append("- " + "c"*i + "\n")
 
 with open("/home/miha/Documents/MAI/DA/lab2-3/solution/input2.txt", "w") as file1:
     file1.writelines(L)
